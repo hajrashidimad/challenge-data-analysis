3e86fadc771256727b5a369f03d04dc28d486198
Ebu <ebu@iBoo.lan>
2020-12-03 16:27:26 +0100

    add multiple function whitch do plot around price, surface and home type

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -19,27 +19,86 @@ class Plot:
     def plot_home_type_by_quantity(self, df):
 
         df['quantity'] = 1
-        test = df.groupby('home_type')['quantity'].sum()
-        test.sort_values['quantity']
-        print(test)
-        test.plot(kind="bar")
+        test = df.groupby(['home_type']).agg({'quantity':sum})
+        res = test.apply(lambda x: x.sort_values(ascending=False).head(3))
+        res.plot(kind="bar")
 
         plt.show()
 
     def plot_surface_of_type_price(self, df): 
 
-        df = df[(df['home_type'] == 'Appartement')]
-        df = df[df['price'] > 1]
+        df = df[(df['home_type'] == 'Appartement') | (df['home_type'] == 'HOUSE') | (df['home_type'] == 'Maison')]
+        df = df[(df['price'] > 1) & (df['price'] < 1000000)]
         df = df[(df['surface_of_land_area'] > 0)]
-        scatter = sns.lmplot(x= 'price', y= 'surface_of_land_area', data= df, fit_reg=False)
-        scatter.set(title='Correlation between prices and surface of appartement')
+        scatter = sns.lmplot(x= 'price', y= 'surface_of_land_area', data= df, fit_reg=False, col='home_type',
+                             hue='home_type')
         plt.xscale('log')
         plt.yscale('log')
-        plt.xlabel('Price of properties')
-        plt.ylabel('Surface of properties')
+        scatter.set_axis_labels('Prices of properties', 'Surface of properties')
+        scatter.set_titles('Correlation of price by home type')
+        
+        plt.show()
+    
+    def proportions_of_home_type(self, df):
+
+        first = (df['home_type']=='Appartement').sum()
+        second = (df['home_type']=='Maison').sum()
+        third = (df['home_type']=='HOUSE').sum()
+        plt.pie([first, second, third],  labels=['Appartement', 'Maison', 'HOUSE'], colors=['red','blue', 'green'], 
+                shadow=False, autopct='%1.1f%%', startangle=100)
+        plt.axis('equal')
+        plt.title('Home type proportions')
+        plt.show()
+    
+    def home_type_price_dispertion(self, df):
+        
+        df = df[(df['home_type'] == 'Appartement') | (df['home_type'] == 'HOUSE') | (df['home_type'] == 'Maison')]
+        df = df[(df['price'] > 1) & (df['price'] < 800000)]
+        sns.set_style('whitegrid')
+        # Violin plot
+        sns.violinplot(x='home_type', y='price', data=df)
+        plt.show()
+    
+    def home_type_surface_dispertion(self, df):
+
+        df = df[(df['home_type'] == 'Appartement') | (df['home_type'] == 'HOUSE') | (df['home_type'] == 'Maison')]
+        df = df[df['surface_of_land_area'] > 0]
+        df = df[(df['surface_of_land_area'] > 1) & (df['surface_of_land_area'] < 2000)]
+        sns.set_style('whitegrid')
+        # Violin plot
+        sns.violinplot(x='home_type', y='surface_of_land_area', data=df)
+        plt.show()
+    
+    def distribution_of_surface(self, df):
+
+        print(df['surface_of_land_area'].mean())
+        df = df[(df['home_type'] == 'Appartement') | (df['home_type'] == 'HOUSE') | (df['home_type'] == 'Maison')]
+        df = df[df['surface_of_land_area'] > 0]
+        first = (df['surface_of_land_area'] > (df['surface_of_land_area'].mean())+500).sum()
+        second = (df['surface_of_land_area'] < (df['surface_of_land_area'].mean())-500).sum()
+        third = ((df['surface_of_land_area'] <= (df['surface_of_land_area'].mean())+500) & 
+                    (df['surface_of_land_area'] <= (df['surface_of_land_area'].mean())+500)).sum()
+        plt.pie([first, second, third],  labels=['Bigger then mean', 'Lesser then mean', 'Mean'], colors=['red','blue', 'green'], 
+                shadow=False, autopct='%1.1f%%', startangle=100)
+        plt.axis('equal')
+        plt.title('Surface distribution')
+        plt.show()
+    
+    def distribution_of_price(self, df):
+
+        print(df['price'].mean())
+        df = df[(df['home_type'] == 'Appartement') | (df['home_type'] == 'HOUSE') | (df['home_type'] == 'Maison')]
+        df = df[df['price'] > 0]
+        first = (df['price'] > (df['price'].mean())+100000).sum()
+        second = (df['price'] < (df['price'].mean())-100000).sum()
+        third = ((df['price'] <= (df['price'].mean())+100000) & 
+                    (df['price'] <= (df['price'].mean())+100000)).sum()
+        plt.pie([first, second, third],  labels=['Bigger then mean', 'Lesser then mean', 'Mean'], colors=['red','blue', 'green'], 
+                shadow=False, autopct='%1.1f%%', startangle=100)
+        plt.axis('equal')
+        plt.title('Price distribution')
         plt.show()
         
-
 df = Cleaning().import_from_csv()
 df = Cleaning().check_space(df)
 df = Cleaning().delete_duplicate(df)
@@ -48,5 +107,10 @@ df = Cleaning().remplace_NaN_value(df)
 
 AnalyseData().how_many_row_and_columns(df)
 AnalyseData().describe_of_values(df)
-Plot().plot_home_type_by_quantity(df)
+#Plot().plot_home_type_by_quantity(df)
 #Plot().plot_surface_of_type_price(df)
+#Plot().proportions_of_home_type(df)
+#Plot().home_type_price_dispertion(df)
+#Plot().home_type_surface_dispertion(df)
+#Plot().distribution_of_surface(df)
+#Plot().distribution_of_price(df)
